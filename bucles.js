7400f09a5820cf393c983bc7f1c0180d57b770ad
DickGrayson <oscaarmayhem@gmail.com>
2024-09-04 18:24:46 -0600

    bucles actualizado

---
+++ bucles.js
@@ -0,0 +1,24 @@
+var suma = 0;
+//suma = suma +1;
+//suma = suma +2;
+//suma = suma +3;
+//suma = suma +4;
+//console.log (suma)
+
+for (var i=0 ; i<10 ; i++){
+    suma = suma +1;
+    console.log (suma);
+}
+
+for (var i=0 ; i<5 ; i++){
+    suma = suma +i;
+    console.log ('variable de interacion: ' , i );
+}
+    console.log ('variable suma: ' , suma);
+
+while (suma < 3){
+    suma = suma +1;
+    console.log (suma);
+}
+
+//PARA EJECUTAR LAS OPERACIONES POR APARTE HAY QUE ELIMINAR LAS ANTERIORES A LAS QUE SE QUIERE EJECUTAR
